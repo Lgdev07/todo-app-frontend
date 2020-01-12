34198198ecd7b9b1574a72187a2cfb83457f4f9f
lgdev07 <luan_saopaulo06@hotmail.com>
2020-01-12 15:59:27 -0300

    [IMP] - Adiciona possibilidade de fazer update e delete das tarefas

--- src/pages/Task/Detail/index.js
+++ src/pages/Task/Detail/index.js
@@ -2,15 +2,35 @@ import React, { useState, useEffect } from 'react'
 import api from '../../../services/api'
 import './styles.css'
 
-export default function TaskDetail({ match }){
+export default function TaskDetail({ match, history }){
 
     const [task, setTask] = useState({})
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+
     const { id } = match.params
 
-    const markTaskAsDone = async () =>{
+    const handleDone = async () =>{
         await api.put(`/tasks/${id}`, {
             active: false
         })
+
+        history.push('/dashboard')
+    }
+
+    const handleUpdate = async () =>{
+        await api.put(`/tasks/${id}`, {
+            name: name || task.name,
+            description: description || task.description,
+        })
+
+        history.push('/dashboard')
+    }
+
+    const handleDelete = async () =>{
+        await api.delete(`/tasks/${id}`)
+
+        history.push('/dashboard')
     }
 
     useEffect(() => {
@@ -27,10 +47,19 @@ export default function TaskDetail({ match }){
 
     return (
         <div className="task-description">
-            <h1>{task.name}</h1>
-            <p>{task.description}</p>
-            <button onClick={markTaskAsDone} className="mark-done">Mark as Done</button>
-            <button className="mark-delete">Delete</button>
+            <input 
+                onChange={event => setName(event.target.value)} 
+                defaultValue={task.name}
+                className="input-name"
+            />
+            <textarea 
+                onChange={event => setDescription(event.target.value)} 
+                defaultValue={task.description}
+                className="input-description"
+            />
+            <button onClick={handleDone} className="mark-done">Mark as Done</button>
+            <button onClick={handleUpdate} className="mark-update">Update</button>
+            <button onClick={handleDelete} className="mark-delete">Delete</button>
         </div>
     )
 }
